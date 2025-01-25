3e6135cc111c084583319ca8c7f1e486333f2091
flexi b <105246299+bignerdmonster@users.noreply.github.com>
2025-01-24 14:36:39 -1000

    Update page.tsx to be more true
    all the things you called me Charly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,67 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Create <span className="text-[hsl(280,100%,70%)]">All the Things Charly Sowers has called me</span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
+            href="https://bigrat.monster"
             target="_blank"
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              "Fat got"
             </div>
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
+            href="https://blm.org"
             target="_blank"
           >
             <h3 className="text-2xl font-bold">Documentation →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+              "White Supremacist"
+            </div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fthomasdonotseal.fandom.com%2Fwiki%2FThe_Holy_Discord_Moderator&psig=AOvVaw0ri8yiMNvj-VGQ8v61TgmW&ust=1737851593901000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCND8_OjPj4sDFQAAAAAdAAAAABAE"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              "Greasy"
+            </div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://upload.wikimedia.org/wikipedia/en/e/e1/PigPen_%28Peanuts%29.png"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              "Dirty"
+            </div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://i.guim.co.uk/img/media/1d7375cb41d9619c68640c972e70844bc8977aa6/475_0_3347_2007/master/3347.jpg?width=465&dpr=1&s=none&crop=none"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              "Neo Nazi"
+            </div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://upload.wikimedia.org/wikipedia/commons/7/7f/Cavi.jpg"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              "Not Sock Wearing"
             </div>
           </Link>
         </div>
